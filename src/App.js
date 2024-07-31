20479ce26d691da27f49a76aa2aca0cad10e43fe
Ubuntu <ubuntu@ip-172-26-13-134.ec2.internal>
2024-07-31 02:07:52 +0000

    added new template and worked on progress bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 'use client';
 import JsonFormatter from 'react-json-formatter'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "assets/theme";
+import MKInput from "components/MKInput";
+import MKButton from "components/MKButton"
+import { io } from 'socket.io-client';
+import MKProgress from "components/MKProgress";
+
+
+
+const socket = io('ws://localhost:5000/', {transports: ["websocket"]});
+
 
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
   const [response, setResponse] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    socket.on('progress', (progress) => {
+      setProgress(progress);
+    });
+
+    return () => {
+      socket.off('progress');
+    };
+  }, []);
 
   const handleSubmit = async(e) => {
     e.preventDefault();
@@ -32,20 +55,24 @@ export default function Home() {
   }
 
   return (
-    <div style={{ padding: '20px' }}>
-      <form onSubmit={handleSubmit}>
-        <input
+   <ThemeProvider theme={theme}>
+   <CssBaseline /> 
+   <div style={{ padding: '20px' }}>
+      <form onSubmit={handleSubmit} style={{ display: 'flex', alignItems: 'center' }}>
+        <MKInput
           type="text"
+	  label =  "Domain"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           required
           style={{ padding: '10px', marginRight: '10px' }}
         />
-        <button type="submit" style={{ padding: '10px' }}>Submit</button>
+        <MKButton variant = "gradient" color = "info" type="submit" style={{ padding: '10px' }}>Submit</MKButton>
       </form>
-      {loading && <p>Loading...</p>}
+      {loading && <MKProgress value={progress} />}
       {response && < JsonFormatter json={response} tabWith={4} jsonStyle={jsonStyle} />}
     </div>
+    </ThemeProvider>
   );
 }
- 
+
