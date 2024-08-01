191b03950dd585896572cdf65b138f139624b103
Ubuntu <ubuntu@ip-172-26-13-134.ec2.internal>
2024-08-01 20:09:12 +0000

    updated python code for readability and speed

--- api/api.py
+++ api/api.py
@@ -3,8 +3,8 @@ from quart_cors import cors
 from urllib.parse import urljoin
 from playwright.async_api import async_playwright
 from bs4 import BeautifulSoup
-#from employees import search
-import requests
+import aiohttp
+import asyncio
 import json
 from supabase import create_client, Client
 import keys
@@ -13,43 +13,41 @@ import keys
 app = Quart(__name__)
 app = cors(app, allow_origin="*")
 
+async def fetch(session, url, payload, headers):
+    async with session.post(url, json=payload, headers=headers) as response:
+        return await response.json()
+
 async def search_employees(domain):
     #check if domain is in the db
     url = keys.url
     key= keys.key
     supabase: Client = create_client(url, key) 
-    response = supabase.table('Employees').select("*").execute()
-    for i in response.data:
-        if domain == i["domain"]:
-            return json.loads(i["employees"])
+    response = supabase.table('Employees').select("*").eq('domain', domain).execute()
+    if response.data:
+        return json.loads(response.data[0]["employees"])
 
     #if not in db, scrape the data from theorg
         
     # get the slug from domain name
     url = "https://prod-graphql-api.theorg.com/graphql"
-    payload = "{\"query\":\"query search($query: String!) {\\n  searchCompanies(query: $query) {\\n    id\\n    slug\\n    name\\n    type\\n    status\\n    extension\\n    verificationType\\n    verified\\n    logoImage {\\n      ...ImageFragment\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment ImageFragment on Image {\\n  endpoint\\n  ext\\n  uri\\n  versions\\n  __typename\\n}\",\"variables\":{\"query\":\""+ domain + "\"}}"
+    payload = {
+        "query": "query search($query: String!) { searchCompanies(query: $query) { id slug name type status extension verificationType verified logoImage { ...ImageFragment __typename } __typename } } fragment ImageFragment on Image { endpoint ext uri versions __typename }",
+        "variables": {"query": domain}
+    }    
     headers = {
     'accept': '*/*',
-    'accept-language': 'en-US,en;q=0.9',
-    'content-type': 'application/json',
+    'content-type' : 'application/json',
     'origin': 'https://theorg.com',
-    'priority': 'u=1, i',
     'referer': 'https://theorg.com/',
-    'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-site',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
   }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    try:
-        slug = response.json()['data']['searchCompanies'][0]['slug']
-    except:
-        return(['test'])
+    async with aiohttp.ClientSession() as session:
+        response = await fetch(session, url, payload, headers)
+        try:
+            slug = response['data']['searchCompanies'][0]['slug']
+        except (KeyError, IndexError):
+            return []
     
     async with async_playwright() as playwright:
         browser = await playwright.chromium.launch(headless=True)
@@ -61,33 +59,20 @@ async def search_employees(domain):
         page = await context.new_page()
         await page.goto("https://theorg.com/org/"+slug+"/")
 
-        totalUsers = []
-        totalPositions = []
-
         soup = BeautifulSoup(await page.content(), "html.parser")
         
-        users = soup.find_all('span', {'class':'PositionCard_name__iERDX'})
-        users = [user.get_text(strip=True) for user in users]
-        
-        positions = soup.find_all('div', {'class':'PositionCard_role__XNUly'})
-        positions = [position.get_text(strip=True) for position in positions]
+        users = [user.get_text(strip=True) for user in soup.find_all('span', {'class':'PositionCard_name__iERDX'})]
 
-        totalUsers.append(users) 
-        totalPositions.append(positions)
+        positions = [position.get_text(strip=True) for position in soup.find_all('div', {'class':'PositionCard_role__XNUly'})]
     
-        #removes all duplicates from the list
-        totalUsers = [item for sublist in totalUsers for item in sublist]
-        totalPositions = [item for sublist in totalPositions for item in sublist]
-        total = dict(zip(totalUsers, totalPositions))
-        employees = {}
-        for key, value in total.items():
-            if key not in employees:
-                employees[key] = value
+        total = dict(zip(users, positions))
+        employees = {key: value for key, value in total.items()}
 
         #push to database
-        if employees != {}:
+        if employees:
             data, count = supabase.table('Employees').insert({"domain": domain, "employees": employees}).execute() 
         
+        await browser.close()
         return employees
 
 
@@ -95,92 +80,66 @@ async def search_employees(domain):
 async def get_domain():
     #get the URL from the post request
     try:
-        competitors = {}
-        overview = {}
         url = await request.get_json()
         url = url.get('domain')
     except:
-        return []
+        return jsonify([])
 
     async with async_playwright() as playwright:
         #setup playwright
         browser = await playwright.chromium.launch(headless=True)
         context = await browser.new_context(user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+        page = await context.new_page()
         try:
             # Open the URL in a new Playwright page
-            page = await context.new_page()
             await page.goto("https://www.similarweb.com/website/"+url+"/competitors/")
-            button_exists = await page.query_selector('.app-more-less-text__button') is not None
-            if button_exists:
+            
+            if await page.query_selector('.app-more-less-text__button'):
                 await page.click('.app-more-less-text__button')
 
             #setup bs4
             soup = BeautifulSoup(await page.content())
             
             #get the url overview
-            domain = soup.find('p', {'class': 'wa-overview__title'})
-            domain = domain.get_text(strip=True) if domain else ""
-
-            description = soup.find('div', {"class": "app-more-less-text"})
-            description = description.get_text(strip=True).replace("Show less", "").strip() if description else ""
-
-            globalRank = soup.find('p', {"class": "wa-rank-list__value"})
-            globalRank = globalRank.get_text(strip=True) if globalRank else ""
-
-            globalRankChange = soup.find('span', {"class": "app-parameter-change app-parameter-change--md app-parameter-change--up"})
-            globalRankChange = globalRankChange.get_text(strip=True) if globalRankChange else ""
-
-            country = soup.find('div', {"class": "wa-rank-list__info"})
-            country = country.get_text(strip=True) if country else ""
-
-            countryRanks = soup.find_all('p', {"class": "wa-rank-list__value"})
-            countryRank = countryRanks[1].get_text(strip=True) if countryRanks and len(countryRanks) > 1 else ""
-
-            totalVisit = soup.find('p', {"class": "engagement-list__item-value"})
-            totalVisit = totalVisit.get_text(strip=True) if totalVisit else ""
+            domain_info = {
+                'domain': soup.find('p', {'class': 'wa-overview__title'}).get_text(strip=True) if soup.find('p', {'class': 'wa-overview__title'}) else '',
+                'description': soup.find('div', {"class": "app-more-less-text"}).get_text(strip=True).replace("Show less", "").strip() if soup.find('div', {"class": "app-more-less-text"}) else '',
+                'globalRank': soup.find('p', {"class": "wa-rank-list__value"}).get_text(strip=True) if soup.find('p', {"class": "wa-rank-list__value"}) else '',
+                'globalRankChange': soup.find('span', {"class": "app-parameter-change app-parameter-change--md app-parameter-change--up"}).get_text(strip=True) if soup.find('span', {"class": "app-parameter-change app-parameter-change--md app-parameter-change--up"}) else '',
+                'country': soup.find('div', {"class": "wa-rank-list__info"}).get_text(strip=True) if soup.find('div', {"class": "wa-rank-list__info"}) else '',
+                'countryRank': soup.find_all('p', {"class": "wa-rank-list__value"})[1].get_text(strip=True) if len(soup.find_all('p', {"class": "wa-rank-list__value"})) > 1 else '',
+                'totalVisits': soup.find('p', {"class": "engagement-list__item-value"}).get_text(strip=True) if soup.find('p', {"class": "engagement-list__item-value"}) else '',
+                'employees': await search_employees(url)            
+                }
 
             #get the competitors overview
-            domains = soup.find_all('a', {"class" : 'wa-competitors-card__website-title'})
-            domains = [domain.get_text(strip=True) for domain in domains]
+            competitors = []
+            domains = [d.get_text(strip=True) for d in soup.find_all('a', {"class": 'wa-competitors-card__website-title'})]
+            descriptions = [d.get_text(strip=True) for d in soup.find_all('p', {"class": 'wa-competitors-card__website-description'}, {'data-test': "total-visits"})]
+            totalVisits = [v.get_text(strip=True) for v in soup.find_all('p', {"class": 'engagement-list__item-value'})][4:][::4]
+            categoryIds = [c.get_text(strip=True) for c in soup.find_all('div', {"class": 'wa-rank-list__info'})][2:][1::2] if len(totalVisits) == 9 else [c.get_text(strip=True) for c in soup.find_all('div', {"class": 'wa-rank-list__info'})][1::2]
+            categoryRanks = [r.get_text(strip=True) for r in soup.find_all('p', {"class": "wa-rank-list__value"})][5:][::3]
+            similarities = [s.get_text(strip=True) for s in soup.find_all('span', {"class": "app-progress wa-competitors-card__affinity-progress"})]
 
-            descriptions = soup.find_all('p', {"class" : 'wa-competitors-card__website-description'}, {'data-test' : "total-visits"})
-            descriptions = [description.get_text(strip=True) for description in descriptions]
+            tasks = [search_employees(domain) for domain in domains]
+            totalEmployees = await asyncio.gather(*tasks)
 
-            totalVisits = soup.find_all('p', {"class" : 'engagement-list__item-value'})
-            totalVisits = [totalVisit.get_text(strip=True) for totalVisit in totalVisits][4:][::4]
+            for domain, description, totalVisit, categoryId, categoryRank, similarity, employee in zip(domains, descriptions, totalVisits, categoryIds, categoryRanks, similarities, totalEmployees):
+                competitors.append({
+                    "domain": domain,
+                    "totalVisits": totalVisit,
+                    "categoryId": categoryId,
+                    "categoryRank": categoryRank,
+                    "description": description,
+                    "similarity": similarity,
+                    "employees": employee
+                })
 
-            categoryIds = soup.find_all('div', {"class": 'wa-rank-list__info'})
-            categoryIds = [categoryId.get_text(strip=True) for categoryId in categoryIds][2:][1::2] if len(totalVisits) == 9 else [categoryId.get_text(strip=True) for categoryId in categoryIds][1::2]
-
-            categoryRanks = soup.find_all('p', {"class" : "wa-rank-list__value"})
-            categoryRanks = [categoryRank.get_text(strip=True) for categoryRank in categoryRanks][5:][::3]
-
-            similarities = soup.find_all('span', {"class" : "app-progress wa-competitors-card__affinity-progress"})
-            similarities = [similarity.get_text(strip=True) for similarity in similarities]
-
-            #get employees for input URL 
-            employee = await search_employees(url)
-
-            #construct dictionary for the main URL
-            overview = {'domain': domain, 'description' : description, 'globalRank' : globalRank, 'globalRankChange' : globalRankChange, 'country':country, 'countryRank':countryRank, "totalVisits" :totalVisit, "employees": employee}
-
-            #get employees for each of the competitors
-            totalEmployees = []
-            for idx, domain in enumerate(domains):
-                try:
-                    employees = await search_employees(domain)
-                    totalEmployees.append(employees)
-                except:
-                    totalEmployees.append([])
-
-            #construct dictionary for the competitors
-            competitors = [{"domain": domain, "totalVisits" : totalVisit, "categoryId" : categoryId, "categoryRank" : categoryRank, "description": description, "similarity" : similarity, "employees" : employee} for domain, description, totalVisit, categoryId, categoryRank, similarity, employee in zip(domains, descriptions, totalVisits, categoryIds, categoryRanks, similarities, totalEmployees)]
-
-            response_data= {'overview': overview, 'competitors': competitors}
+            response_data= {'overview': domain_info, 'competitors': competitors}
+            print(json.dumps(response_data, indent=4))
             return json.dumps(response_data, indent=4)
 
         except Exception as e:
-            return(f'Cannot extract data from {url}.')
+            return(f'Cannot extract data from {url}. The error is {e}')
         finally:
             await page.close()
-
